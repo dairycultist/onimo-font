6afe223065e68d78c67f427c9e3f899994695750
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2025-03-12 17:37:05 -0400

    Initial commit

---
+++ grid_script.py
@@ -0,0 +1,27 @@
+# https://namethattech.wordpress.com/2017/03/22/how-to-make-a-snap-to-grid-in-fontforge/comment-page-1/
+
+# This script outputs instructions to draw a snap grid in FontForge.
+# python3 script.py > grid.txt
+# Then copy these instructions just below the only occurence of ‘Grid’ in a .sfd (FontForge file).
+
+GRID_STEP = 64
+SNAP_STEP = 32
+MIN_X = -224
+MAX_X = 1024
+MIN_Y = MIN_X
+MAX_Y = MAX_X
+
+# Lines
+for y in range(MIN_Y, MAX_Y + 1, GRID_STEP):
+	print(f"{MIN_X} {y} m 1")
+	print(f"{MAX_X} {y} l 1")
+
+# Lines
+for x in range(MIN_X, MAX_X + 1, GRID_STEP):
+	print(f"{x} {MIN_Y} m 1")
+	print(f"{x} {MAX_Y} l 1")
+
+# By moving the pen, we can add snap points without drawing anything
+for y in range(MIN_Y, MAX_Y + 1, SNAP_STEP):
+	for x in range(MIN_X, MAX_X + 1, SNAP_STEP):
+		print(f"{x} {y} m 1")
